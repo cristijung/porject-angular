b770b31d612097631a2c45c09f78a1e090e797af
Cristina Jung <cristijung@gmail.com>
2023-04-26 17:14:54 -0300

    inicio

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Pafúncio Co-working';
+  stitle = 'O melhor estaço para o trabalho!'
+  footer = 'Desenvolvido por mim'
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ForumComponent } from './forum/forum.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    ForumComponent,
+    GaleriaComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
