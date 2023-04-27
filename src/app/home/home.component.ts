d381fcd5deaee5b4c25a366d6b924170a84c3e9b
Cristina Jung <cristijung@gmail.com>
2023-04-27 16:58:32 -0300

    começo do material angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+
+  title = 'Seja Bem Vindo';
+  description = 'Imagem de Co-Working';
+  url = 'https://casacor.abril.com.br/wp-content/uploads/sites/7/2021/08/urban-airport-08.jpg?quality=80&strip=info&w=1024';
 
 }
